44ffe2ba85dac89c445d6639aa977d3161e6f8f6
Pavlo Kovalchuk <matara@meta.ua>
2015-02-27 12:23:06 +0100

    implement additional test for tree levels

--- spec/algorithms/tree_levels_spec.rb
+++ spec/algorithms/tree_levels_spec.rb
@@ -1,30 +1,44 @@
 require 'spec_helper'
 
+TreeNode = Struct.new(:value, :left, :right)
 describe Algorithms::TreeLevels do
 
   before do
     @tl = Algorithms::TreeLevels
-
-    TreeNode = Struct.new(:value, :left, :right)
-    @tree = TreeNode.new(0,
-                         TreeNode.new(1,
-                                      nil,
-                                      TreeNode.new(2, nil, nil)
-
-                         ),
-                         TreeNode.new(3,
-                                      TreeNode.new(4, nil, nil),
-                                      TreeNode.new(5,
-                                                   TreeNode.new(6, nil, nil),
-                                                   nil)
-                         )
-
-    )
   end
 
-  it 'should return correct value' do
-    value = [[0], [1, 3], [2, 4, 5], [6]]
-    expect(@tl.tree_levels(@tree)).to eq(value)
+  describe 'default' do
+    before do
+      @tree = TreeNode.new(0,
+                           TreeNode.new(1,
+                                        nil,
+                                        TreeNode.new(2, nil, nil)
+
+                           ),
+                           TreeNode.new(3,
+                                        TreeNode.new(4, nil, nil),
+                                        TreeNode.new(5,
+                                                     TreeNode.new(6, nil, nil),
+                                                     nil)
+                           )
+
+      )
+    end
+
+    it 'should return correct value' do
+      expect(@tl.tree_levels(@tree)).to eq([[0], [1, 3], [2, 4, 5], [6]])
+    end
   end
 
+  describe 'custom' do
+    it 'should return correct value v1' do
+      tree = TreeNode.new(0, nil, nil)
+      expect(@tl.tree_levels(tree)).to eq([[0]])
+    end
+
+    it 'should return correct value v2' do
+      tree = TreeNode.new(0, TreeNode.new(0, nil, nil), TreeNode.new(0, nil, nil))
+      expect(@tl.tree_levels(tree)).to eq([[0], [0,0]])
+    end
+  end
 end
