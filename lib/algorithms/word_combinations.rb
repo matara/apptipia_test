f5780a987c53af75c7d627a0adf8c546a406391d
Pavlo Kovalchuk <matara@meta.ua>
2015-02-27 12:16:59 +0100

    fix word_combinations

--- lib/algorithms/word_combinations.rb
+++ lib/algorithms/word_combinations.rb
@@ -3,13 +3,33 @@ module Algorithms
     class << self
 
       def find_possible_combinations(dictionary, target)
-        combinations = dictionary
-        target.size.times do
-          combinations += dictionary.product(combinations)
+        @global_target = target
+        @variants = []
+        variants_for(dictionary * target.size)
+        @variants.uniq.select { |e| e.join == @global_target }.map { |e| e.join(' ') }.sort
+      end
+
+      private
+      def without_last(array)
+        array.take(array.size - 1)
+      end
+
+      def variants_for(combinations)
+        combinations.each do |element|
+          arr = []
+          arr << [element] if @global_target.index(element) == 0
+          @variants.each do |variant|
+            if @global_target.index(variant.join + element) == 0
+              variant << element
+            elsif @global_target.index(without_last(variant).join + element) == 0
+              arr << without_last(variant) + [element]
+            end
+          end
+          @variants += arr
+          index = combinations.index(element)
+          combinations.delete_at(index) if index >= 0
+          variants_for(combinations)
         end
-        combinations = combinations.uniq.map{|e| e.is_a?(Array) ? e.flatten : e}
-        combinations = combinations.select {|e| e.is_a?(Array) ? e.join == target : e == target}
-        combinations.map {|w| w.join(' ')}.sort
       end
 
     end
--- spec/algorithms/word_combinations_spec.rb
+++ spec/algorithms/word_combinations_spec.rb
@@ -4,17 +4,59 @@ describe Algorithms::WordCombinations do
 
   before do
     @wc = Algorithms::WordCombinations
-    @dictionary = ['a', 'b', 'c', 'ab', 'abc']
   end
 
-  it 'should return correct value' do
-    target = 'aabc'
-    expect(@wc.find_possible_combinations(@dictionary, target)).to eq(['a abc', 'a a b c', 'a ab c'].sort)
+  describe 'default' do
+    before do
+      @dictionary = ['a', 'b', 'c', 'ab', 'abc']
+    end
+
+    it 'should return correct value' do
+      target = 'aabc'
+      expect(@wc.find_possible_combinations(@dictionary, target)).to eq(['a abc', 'a a b c', 'a ab c'].sort)
+    end
+
+    it 'should return empty array' do
+      target = 'aabcx'
+      expect(@wc.find_possible_combinations(@dictionary, target)).to eq([])
+    end
   end
 
-  it 'should return empty array' do
-    target = 'aabcx'
-    expect(@wc.find_possible_combinations(@dictionary, target)).to eq([])
-  end
+  describe 'custom' do
+    it 'should return correct value v1' do
+      dictionary = ['a', 'aa', 'b', 'ab']
+      target = 'aaa'
+      expect(@wc.find_possible_combinations(dictionary, target)).to eq(['a a a', 'a aa', 'aa a'].sort)
+    end
 
+    it 'should return correct value v2' do
+      dictionary = ['a', 'aa', 'aaa', 'b', 'ab']
+      target = 'aaa'
+      expect(@wc.find_possible_combinations(dictionary, target)).to eq(['a a a', 'a aa', 'aa a', 'aaa'].sort)
+    end
+
+    it 'should return correct value v3' do
+      dictionary = ['a']
+      target = 'aaa'
+      expect(@wc.find_possible_combinations(dictionary, target)).to eq(['a a a'].sort)
+    end
+
+    it 'should return correct value v4' do
+      dictionary = ['a']
+      target = 'a'
+      expect(@wc.find_possible_combinations(dictionary, target)).to eq(['a'].sort)
+    end
+
+    it 'should return correct value v5' do
+      dictionary = ['a', 'b', 'c']
+      target = ''
+      expect(@wc.find_possible_combinations(dictionary, target)).to eq([])
+    end
+
+    it 'should return correct value v6' do
+      dictionary = ['a', 'b', 'ab', 'ba']
+      target = 'abba'
+      expect(@wc.find_possible_combinations(dictionary, target)).to eq(['a b b a', 'a b ba', 'ab b a', 'ab ba'].sort)
+    end
+  end
 end
